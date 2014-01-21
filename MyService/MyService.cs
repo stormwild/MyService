97bcab17d6ded537730c2e4bc204b75e49cece24
Alexander Torrijos <artorrijos@gmail.com>
2014-01-21 21:45:38 +0800

    Initial commit

---
+++ MyService/MyService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+
+namespace MyService
+{
+    public partial class MyService : ServiceBase
+    {
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Start();
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        protected void Start()
+        {
+            Thread thread = new Thread(Task);
+            thread.Start();
+        }
+
+        protected void Task()
+        {
+            // do a task here
+            // create a thread
+            while (true)
+            {
+                // do something
+            }
+        }
+    }
+}
